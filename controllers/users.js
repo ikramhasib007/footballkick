af3f8d2d3bb83fc533d0073e5f2f8c0af1eab9ec
ikram <ikramhasib007@gmail.com>
2017-12-07 18:08:11 +0600

    added theme

--- controllers/users.js
+++ controllers/users.js
@@ -4,10 +4,15 @@ module.exports = function(_){
 	return {
 		SetRouting: function(router){
 			router.get('/', this.indexPage);
+			router.get('/signup', this.signupPage);
 		},
 
 		indexPage: function(req, res){
-			return res.render('index', {test: 'This is a test'});
+			return res.render('index');
+		},
+
+		signupPage: function(req, res){
+			return res.render('signup');
 		}
 	}
 }
